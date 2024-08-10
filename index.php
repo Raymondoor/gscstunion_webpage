a3b0b1fe76dd8f9bc306f39374da1c45958bba49
Raymond <109900459+Raymondoor@users.noreply.github.com>
2024-08-11 07:43:27 +0900

    clamp fix

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "{$dir_back}backend/app/views/header.php";
     <main>
         <div id="home-banner">
             <h2>GSC Stunion</h2>
-            <h3>地球社会共生学部学生連合</h3>
+            <h3><ruby>地球社会共生学部学生連合<rt>ちきゅうしゃかいきょうせいがくぶがくせいれんごう</rt><rp>ちきゅうしゃかいきょうせいがくぶがくせいれんごう</rp></ruby></h3>
             <h4>青山学院大学</h4>
         </div>
         <div id="aboutus">
