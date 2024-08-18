c0d7133fd339a1ca9744edfc3ecf26eac31648cb
Raymond <109900459+Raymondoor@users.noreply.github.com>
2024-08-19 02:19:10 +0900

    admin style

--- admin/index.php
+++ admin/index.php
@@ -11,19 +11,26 @@ include "{$dir_back}backend/app/views/header.php";
 ?>
 
     <main>
-        <h2>admin page</h2>
+        <h2 id="adh2">編集ページ</h2>
+        <h4 id="adh4">新しい記事を書く</h4>
         <div id="addScriptform">
             <form action="../api/add_article.php" method="post" enctype="multipart/form-data">
-                <label>タイトル：</label><br>
-                <input type="text" name="title" id="title" placeholder="Title" autofocus><br>
-                <label for="main">本文：</label><br>
+                <label>タイトル（必須）：</label><br>
+                <input type="text" name="title" id="title" placeholder="タイトル" autofocus><br>
+                <label for="main">本文（必須）：</label><br>
                 <textarea id="main" name="main" rows="10" cols="30" required></textarea><br>
                 <label>リンク：</label><br>
-                <input type="text" name="url" id="url" placeholder="URL"><br>
-                <label>画像ファイル：</label><br>
-                <input type="file" name="media" id="media" placeholder="Image"><br>
+                <input type="text" name="url" id="url" placeholder="リンク"><br>
+                <label>ファイル（画像を選択できます）：</label><br>
+                <input type="file" name="media" id="media"><br>
                 <button type="submit">投稿</button>
             </form>
+        </div>
+        <div id="adexm">
+            <h2 id="patitle">サンプル記事タイトル</h2>
+            <p id="padate">20XX-01-01</p>
+            <p id="pamain">この記事はサンプルです。投稿した内容はこのように出力されます。リンクと画像は一つまで掲載することが出来ます。日付は投稿した当日のものが自動的に反映されます。画像には黒い縁が付きます。</p>
+            <img src="../assets/images/icon/GSCstunionlogo.jpg">
         </div>
     </main>
 
