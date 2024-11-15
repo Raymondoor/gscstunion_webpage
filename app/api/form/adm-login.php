1e994295293c95575597ee2c6a794a6be0c6640d
raymond1206 <109900459+Raymondoor@users.noreply.github.com>
2024-11-15 10:40:09 +0900

    morn 1115

--- app/api/form/adm-login.php
+++ app/api/form/adm-login.php
@@ -1,13 +1,16 @@
 <?php
 require_once (__DIR__ . '/../general/DIR.php');
 require_once (API_DIR . '/general/DATABASE_PROCESS.php');
+require_once (API_DIR . '/general/TEXT_FORMAT.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!isset($_POST['csrf']) || !hash_equals($_SESSION['csfr'], $_POST['csfr'])) {
         die('Invalid Access');
     }
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
-        $query = new DatabaseStatement("SELECT * FROM user WHERE id = :id");
-        $result = $query->Operation([':id' => 1]);
+        $username = sanitize_text($_POST['username']);
+        $password = sanitize_text($_POST['password']);
+        $query = new DatabaseStatement("SELECT * FROM user WHERE username = :username");
+        $result = $query->Operation([':username' => 'username']);
     }
 }
