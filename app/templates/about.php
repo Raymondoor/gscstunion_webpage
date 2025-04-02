c559e7c39ba9e62869695ad3248ecc81478d39ef
raymond1206 <109900459+Raymondoor@users.noreply.github.com>
2025-04-02 17:30:17 +0900

    some css

--- app/templates/about.php
+++ app/templates/about.php
@@ -1,9 +1,9 @@
 <div id="aboutnav">
     <ul>
         <li><a href="<?=APP_URL?>/about/">私たちについて</a></li>
-        <li><a href="<?=APP_URL?>/about/report/">年次報告</a></li>
         <li><a href="<?=APP_URL?>/about/sns/">ソーシャルメディア</a></li>
         <li><a href="<?=APP_URL?>/about/outlink/">外部リンク</a></li>
+        <li><a href="<?=APP_URL?>/about/report/">年次報告</a></li>
         <li><a href="<?=APP_URL?>/about/location/">所在地</a></li>
         <li><a href="<?=APP_URL?>/about/policy/">各種方針</a></li>
         <li><a href="<?=APP_URL?>/about/license/">利用規約・ライセンス</a></li>
--- app/templates/footer.php
+++ app/templates/footer.php
@@ -17,6 +17,7 @@
             <ul>
                 <li><a href="<?=APP_URL?>/about/sns/">ソーシャルメディア</a></li>
                 <li><a href="<?=APP_URL?>/about/outlink/">外部リンク</a></li>
+                <li><a href="<?=APP_URL?>/about/report/">年次報告</a></li>
                 <li><a href="<?=APP_URL?>/about/location/">所在地</a></li>
                 <li><a href="<?=APP_URL?>/about/policy/">各種方針</a></li>
                 <li><a href="<?=APP_URL?>/about/license/">利用規約・ライセンス</a></li>
