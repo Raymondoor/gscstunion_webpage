2f148a54947364f8331e1e88b0ab52d64dd4f90a
raymond1206 <109900459+Raymondoor@users.noreply.github.com>
2025-04-09 22:38:26 +0900

    nav link fix

--- app/templates/header.php
+++ app/templates/header.php
@@ -33,8 +33,8 @@
                     <h3>INFORMATION</h3>
                     <hr>
                     <ul>
-                        <li><a href="<?=APP_URL?>/about/sns">ソーシャルメディア</a></li>
-                        <li><a href="<?=APP_URL?>/about/outlink">外部リンク</a></li>
+                        <li><a href="<?=APP_URL?>/about/sns/">ソーシャルメディア</a></li>
+                        <li><a href="<?=APP_URL?>/about/outlink/">外部リンク</a></li>
                         <li><a href="<?=APP_URL?>/about/location/">所在地</a></li>
                         <li><a href="<?=APP_URL?>/about/policy/">各種方針</a></li>
                         <li><a href="<?=APP_URL?>/about/license/">利用規約・ライセンス</a></li>
