9e7f420cdeaa39b0eb5a44828aa8e67bbafe2823
raymond1206 <109900459+Raymondoor@users.noreply.github.com>
2025-04-19 10:43:00 +0900

    quill hr path fix

--- assets/script/lib/ADMIN-ARTICLE-NEW.js
+++ assets/script/lib/ADMIN-ARTICLE-NEW.js
@@ -76,7 +76,7 @@ document.querySelector('.ql-youtube').appendChild(youtubeImg);
 
 document.querySelector('.ql-divider').innerHTML = ''; // Clear default content
 const hrImg = document.createElement('img');
-hrImg.src = '../../../assets/image/share/bits/hr.png'; // Icon URL
+hrImg.src = '../../assets/image/share/bits/hr.png'; // Icon URL
 hrImg.alt = 'Dividing Line';
 hrImg.style.width = '18px'; // Match typical Quill icon size
 hrImg.style.height = '18px';
